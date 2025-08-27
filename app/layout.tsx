b31438de5b5c13017580504d54ff9fecc0d3811d
vimalbhesaniya <vimalbhesaniya007@gmail.com>
2025-08-27 23:32:37 +0530

    feat: Added husky, eslint and tailwind setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
-	return (
-		<html>
-			<body>{children}</body>
-		</html>
-	);
+import { Poppins } from "next/font/google";
+import "./global.css";
+import { PropsWithChildren } from "react";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400"], // pick weights you need
+  variable: "--font-poppins",
+});
+
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <html lang="en" className={poppins.variable}>
+      <body className="font-sans">{children}</body>
+    </html>
+  );
 }
