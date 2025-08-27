c7faa1ddb42d1b265fdc767ac44cba4108463cb4
arman-rasool <armanrasool@appscrip.co>
2025-08-27 09:33:46 +0530

    add-more-depenedencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
+import "./global.css";
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
 	return (
-		<html>
+		<html className="bg-red-500">
 			<body>{children}</body>
 		</html>
 	);
