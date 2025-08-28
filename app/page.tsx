5d89f7e038d3c0e03928d4aa42b9a872c11e81ce
arman-rasool <armanrasool@appscrip.co>
2025-08-28 23:46:05 +0530

    added-fonts-colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1>Hello Ride Booking App 🚖</h1>;
+  return <h1 className="text-red-500">Hello Ride Booking App 🚖</h1>;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,19 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./containers/**/*.{js,ts,jsx,tsx}",
-    "./shared/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-poppins)", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
+
+export default config;
