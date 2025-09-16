083b7a5b80fc05c03c1de4e581622c8442295d39
arman-rasool <armanrasool@appscrip.co>
2025-09-16 23:50:58 +0530

    axios-interceptor-added

---
+++ services/interceptor.ts
@@ -0,0 +1,80 @@
+import axios, { AxiosError, AxiosInstance, InternalAxiosRequestConfig, AxiosResponse } from "axios";
+import { getCookie, setCookie, clearCookie } from "./cookies-helper";
+
+interface RefreshResponse {
+  accessToken: string;
+}
+
+const api: AxiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
+});
+
+let isRefreshing = false;
+let refreshQueue: Array<(token: string) => void> = [];
+
+// Request Interceptor
+api.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    const token = getCookie("accessToken");
+    if (token && config.headers) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error: AxiosError) => Promise.reject(error)
+);
+
+// Response Interceptor
+api.interceptors.response.use(
+  (response: AxiosResponse) => response,
+  async (error: AxiosError) => {
+    const originalRequest = error.config as InternalAxiosRequestConfig & { _retry?: boolean };
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      if (isRefreshing) {
+        return new Promise((resolve) => {
+          refreshQueue.push((token: string) => {
+            if (originalRequest.headers) {
+              originalRequest.headers["Authorization"] = `Bearer ${token}`;
+            }
+            resolve(api(originalRequest));
+          });
+        });
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+
+      try {
+        const refreshToken = getCookie("refreshToken");
+        const resp = await axios.post<RefreshResponse>(
+          `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api"}/auth/refresh`,
+          { refreshToken }
+        );
+
+        const newAccessToken = resp.data.accessToken;
+        setCookie("accessToken", newAccessToken, 1);
+
+        refreshQueue.forEach((cb) => cb(newAccessToken));
+        refreshQueue = [];
+
+        if (originalRequest.headers) {
+          originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+        }
+
+        return api(originalRequest);
+      } catch (err) {
+        clearCookie();
+        clearCookie();
+        window.location.href = "/login";
+        return Promise.reject(err);
+      } finally {
+        isRefreshing = false;
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
